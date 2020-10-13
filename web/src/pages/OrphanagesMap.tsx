ea0fb4e58607886c78a9eedc893fa9c13dc14cfd
FIRST_NAME LAST_NAME <felipe.brito016@gmail.com>
2020-10-13 12:13:12 -0300

    map-integration

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { FiPlus } from 'react-icons/fi'
+import { Map, TileLayer } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
 
 import '../styles/pages/orphanages-map.css'
 
@@ -21,6 +23,14 @@ function OrphanagesMap(){
                     <strong>Ceará</strong>
                 </footer>
             </aside>
+
+            <Map
+            center={[-3.7453126,-38.4958464]}
+            zoom={15}
+            style={{ width: '100%', height: '100%' }}
+            >
+                <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}/>
+            </Map>
 
             <div></div>
 
