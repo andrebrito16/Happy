67697737a2adb17e88b98644661bfc62a10757e0
FIRST_NAME LAST_NAME <felipe.brito016@gmail.com>
2020-10-13 22:23:28 -0300

    css import

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { FiPlus } from 'react-icons/fi'
 import { Map, TileLayer } from 'react-leaflet'
-import '~leaflet/dist/leaflet.css';
+import "leaflet/dist/leaflet.css";
 import '../styles/pages/orphanages-map.css';
 
 import { Link } from 'react-router-dom'
